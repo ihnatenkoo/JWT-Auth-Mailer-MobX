e70ceaf4710819749d9e89b553d34d93d24d9e00
Dmytro <ihnatenko.dev@gmail.com>
2022-11-16 18:44:09 +0200

    Use middlewares, connect mongo

--- server/index.js
+++ server/index.js
@@ -2,14 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const monsgoose = require('monsgoose');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 const start = async () => {
 	try {
+		await mongoose.connect(process.env.DB_URL);
 		app.listen(PORT, () => console.log(`Server was started on port ${PORT}`));
 	} catch (error) {
 		console.log(error);
