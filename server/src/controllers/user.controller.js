e3554a6609e0c4dde821fdaddb38df03ce5844d3
Dmytro <ihnatenko.dev@gmail.com>
2022-11-18 17:16:14 +0200

    Create user.controller

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,31 @@
+class UserController {
+	async register(req, res, next) {
+		try {
+		} catch (error) {}
+	}
+
+	async login(req, res, next) {
+		try {
+		} catch (error) {}
+	}
+
+	async logout(req, res, next) {
+		try {
+		} catch (error) {}
+	}
+	async activate(req, res, next) {
+		try {
+		} catch (error) {}
+	}
+	async refresh(req, res, next) {
+		try {
+		} catch (error) {}
+	}
+	async getUsers(req, res, next) {
+		try {
+			res.json(['1', '2']);
+		} catch (error) {}
+	}
+}
+
+module.exports = new UserController();
--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,14 +1,13 @@
 const Router = require('express');
+const UserController = require('../controllers/user.controller');
 
 const authRouter = new Router();
 
-authRouter.post('/register', (req, res) => {
-	res.send('ok');
-});
-authRouter.post('/login');
-authRouter.post('/logout');
-authRouter.get('/activate/:link');
-authRouter.get('/refresh');
-authRouter.get('/users');
+authRouter.post('/register', UserController.register);
+authRouter.post('/login', UserController.login);
+authRouter.post('/logout', UserController.logout);
+authRouter.get('/activate/:link', UserController.activate);
+authRouter.get('/refresh', UserController.refresh);
+authRouter.get('/users', UserController.getUsers);
 
 module.exports = authRouter;
