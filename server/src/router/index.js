d5eb24b106edd435f8449bc2f55dcf6277aae728
Dmytro <ihnatenko.dev@gmail.com>
2022-11-25 21:22:07 +0200

    Create auth.middleware

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,6 +1,7 @@
 const Router = require('express');
 const UserController = require('../controllers/user.controller');
 const { body } = require('express-validator');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 const authRouter = new Router();
 
@@ -20,6 +21,6 @@ authRouter.post(
 authRouter.post('/logout', UserController.logout);
 authRouter.get('/activate/:link', UserController.activate);
 authRouter.get('/refresh', UserController.refresh);
-authRouter.get('/users', UserController.getUsers);
+authRouter.get('/users', authMiddleware, UserController.getUsers);
 
 module.exports = authRouter;
