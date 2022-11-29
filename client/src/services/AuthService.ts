34c06b2c366f6fc62544d19256507d2ac4f77cec
Dmytro <ihnatenko.dev@gmail.com>
2022-11-29 14:40:10 +0200

    Client: create AuthService

---
+++ client/src/services/AuthService.ts
@@ -0,0 +1,23 @@
+import { AxiosResponse } from 'axios';
+import $api from '../http';
+import { AuthResponse } from '../types/authResponse';
+
+export class AuthService {
+	static async login(
+		email: string,
+		password: string
+	): Promise<AxiosResponse<AuthResponse>> {
+		return $api.post<AuthResponse>('/auth/login', { email, password });
+	}
+
+	static async registration(
+		email: string,
+		password: string
+	): Promise<AxiosResponse<AuthResponse>> {
+		return $api.post<AuthResponse>('/auth/registration', { email, password });
+	}
+
+	static async logout(): Promise<void> {
+		return $api.post('/auth/logout');
+	}
+}
