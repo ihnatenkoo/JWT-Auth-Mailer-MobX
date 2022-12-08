0d1c6ce5c79013b4977b7ee60f404c6b3984d37d
Dmytro <ihnatenko.dev@gmail.com>
2022-12-08 21:24:31 +0200

    Impl res.interceptor for resresh unauthorized status

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { AuthResponse } from '../types/authResponse';
 
 export const API_URL = 'http://localhost:5000';
 
@@ -13,5 +14,36 @@ $api.interceptors.request.use((config) => {
 	}
 	return config;
 });
+
+$api.interceptors.response.use(
+	(config) => {
+		return config;
+	},
+	async (error) => {
+		const originalRequest = error.config;
+
+		if (
+			error.response.status == 401 &&
+			error.config &&
+			!originalRequest._isRetry
+		) {
+			originalRequest._isRetry = true;
+			try {
+				const response = await axios.get<AuthResponse>(
+					`${API_URL}/auth/refresh`,
+					{
+						withCredentials: true,
+					}
+				);
+				localStorage.setItem('token', response.data.accessToken);
+				return $api.request(originalRequest);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+
+		throw error;
+	}
+);
 
 export default $api;
