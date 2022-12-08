71a5ba81588918e4a2abf9dcb8e64fc2b5db30f5
Dmytro <ihnatenko.dev@gmail.com>
2022-12-08 20:10:13 +0200

    Check if user logged in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import { observer } from 'mobx-react-lite';
-import { FC, useContext } from 'react';
+import { FC, useContext, useEffect } from 'react';
 import Form from './components/Form/Form';
 import UserInfo from './components/UserInfo/UserInfo';
 import { Context } from './main';
 
 const App: FC = () => {
 	const { store } = useContext(Context);
+
+	useEffect(() => {
+		if (localStorage.getItem('token')) {
+			store.checkAuth();
+		}
+	}, []);
 
 	if (store.isAuth) {
 		return (
