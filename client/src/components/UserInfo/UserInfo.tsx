72169fd25519ea633a6f53a2292427c738f8a847
Dmytro <ihnatenko.dev@gmail.com>
2022-12-08 21:05:58 +0200

    Impl getUsers in <UserInfo/>

--- client/src/components/UserInfo/UserInfo.tsx
+++ client/src/components/UserInfo/UserInfo.tsx
@@ -1,17 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Context } from '../../main';
+import { UserService } from '../../services/UserService';
+import { IUser } from '../../types/IUser';
 
 const UserInfo = () => {
+	const [users, setUsers] = useState<IUser[] | []>([]);
 	const {
 		store,
 		store: { user },
 	} = useContext(Context);
 
+	const getUsers = async () => {
+		try {
+			const response = await UserService.fetchUsers();
+			setUsers(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<div>
 			<div>{user.email}</div>
 			<div>{`Activated: ${user.isActivated}`}</div>
+			<button
+				onClick={() => getUsers()}
+				style={{ display: 'block', marginBottom: '10px' }}
+			>
+				Show users
+			</button>
+			{!!users.length && (
+				<ul>
+					{users.map((u) => (
+						<li key={u.email}>{u.email}</li>
+					))}
+				</ul>
+			)}
 			<button onClick={() => store.logout()}>Logout</button>
 		</div>
 	);
